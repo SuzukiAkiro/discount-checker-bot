5a7fd578b5be6e7e437321c36f99bb3e1deb9588
Vladislav Vasilev <akiro_jpg@vk.com>
2023-11-04 01:15:14 +0500

    Created base add command without validation

--- main.py
+++ main.py
@@ -1,7 +1,13 @@
 import logging
 import os
 
-from telegram.ext import ApplicationBuilder, CommandHandler, MessageHandler, filters
+from telegram.ext import (
+    ApplicationBuilder,
+    CommandHandler,
+    MessageHandler,
+    filters,
+    ConversationHandler,
+)
 
 from exceptions import TokenInvalid
 from src import commands
@@ -14,14 +20,25 @@ TELEGRAM_TOKEN = os.getenv("TELEGRAM_API_TOKEN")
 if not TELEGRAM_TOKEN:
     raise TokenInvalid()
 
-
 if __name__ == "__main__":
     application = ApplicationBuilder().token(TELEGRAM_TOKEN).build()
-    list_handler = CommandHandler("tmp", commands.list)
+    list_handler = CommandHandler("list", commands.list_command)
     start_handler = CommandHandler("start", commands.start)
     help_handler = CommandHandler("help", commands.start)
-    echo_handler = MessageHandler(filters.TEXT & ~(filters.COMMAND), commands.echo)
+    conv_handler = ConversationHandler(
+        entry_points=[CommandHandler("add", commands.add)],
+        states={
+            commands.ENTER_TEXT: [
+                MessageHandler(filters.TEXT & ~(filters.COMMAND), commands.save_text)
+            ],
+        },
+        fallbacks=[list_handler, start_handler, help_handler],
+    )
+
+    # item_handler = MessageHandler(filters.TEXT & ~(filters.COMMAND), commands.handle_item_url)
+
     application.add_handler(start_handler)
     application.add_handler(help_handler)
     application.add_handler(list_handler)
+    application.add_handler(conv_handler)
     application.run_polling()
--- src/commands.py
+++ src/commands.py
@@ -1,10 +1,11 @@
 from telegram import Update
-from telegram.ext import ContextTypes
+from telegram.ext import ContextTypes, ConversationHandler
 
 from src import messages
 from src.db_funcs import BotDB
 
 db = BotDB("bot.sql")
+ENTER_TEXT = 0
 
 
 async def start(update: Update, context: ContextTypes.DEFAULT_TYPE):
@@ -13,16 +14,27 @@ async def start(update: Update, context: ContextTypes.DEFAULT_TYPE):
     await context.bot.send_message(chat_id=update.effective_chat.id, text=messages.HELP)
 
 
-async def list(update: Update, context: ContextTypes.DEFAULT_TYPE):
-    result = db.parse_watchlist(update.effective_chat.id)
+async def list_command(update: Update, context: ContextTypes.DEFAULT_TYPE):
+    result = db.parse_watchlist(user_id=update.effective_chat.id)
     answer = ""
     for tuples in result:
         url, price = tuples
         answer += f"Товар: {url}\n Цена: {price}\n\n"
-    await context.bot.send_message(chat_id=update.effective_chat.id, text=answer)
+    if answer:
+        await context.bot.send_message(chat_id=update.effective_chat.id, text=answer)
+    else:
+        await context.bot.send_message(chat_id=update.effective_chat.id, text=result)
 
 
-async def echo(update: Update, context: ContextTypes.DEFAULT_TYPE):
+async def add(update: Update, context: ContextTypes.DEFAULT_TYPE):
+    await context.bot.send_message(chat_id=update.effective_chat.id, text=messages.ADD)
+    return ENTER_TEXT
+
+
+async def save_text(update: Update, context: ContextTypes.DEFAULT_TYPE):
+    msg = update.message.text
+    db.add_item(msg, update.effective_chat.id)
     await context.bot.send_message(
-        chat_id=update.effective_chat.id, text=update.message.text
+        chat_id=update.effective_chat.id, text=messages.ADD_SUCCES
     )
+    return ConversationHandler.END
